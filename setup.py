7cad1509d0a8e346b0812754b6960512fd25ae98
wmobley <wmobley@tamu.edu>
2020-04-27 12:52:27 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
 
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
-    install_requires=["scikit-learn", "numpy", "pandas", "rasterio","joblib"],
+    install_requires=["scikit-learn==0.22.2post1", "numpy", "pandas", "rasterio","joblib"],
 
 
     # metadata to display on PyPI
