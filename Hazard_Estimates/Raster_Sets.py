89fb4371c15a7ff0983d71dbeb451a599bec4c71
wmobley <wmobley@tamu.edu>
2020-04-30 09:09:58 -0500

    Update Raster_Sets.py

--- Hazard_Estimates/Raster_Sets.py
+++ Hazard_Estimates/Raster_Sets.py
@@ -80,7 +80,7 @@ class raster_sets:
         predictions =  rf.ascii_raster()
         gc.collect()
         values = psutil.virtual_memory()
-        chunks = 8*(int(df.memory_usage(deep=True).sum()/values.available)+1)
+        chunks = (int(df.memory_usage(deep=True).sum()/values.available)+1)
         print("chunks", chunks)
         print(p.memory_percent())
         split_data = np.array_split(df, chunks)
