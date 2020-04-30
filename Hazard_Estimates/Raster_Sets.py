6e644a5022b7fb9ddd1d960509c5a5106d161717
wmobley <wmobley@tamu.edu>
2020-04-30 09:49:31 -0500

    Update Raster_Sets.py

--- Hazard_Estimates/Raster_Sets.py
+++ Hazard_Estimates/Raster_Sets.py
@@ -90,7 +90,7 @@ class raster_sets:
 
         predictions.asciiFile = np.array([])
         for data in split_data:
-            print(data.values)
+           
             predictions.asciiFile = np.concatenate( (predictions.asciiFile, np.where(data[ignore_column] != nodata, model.predict_proba(data.values)[:, 1], -9999)), axis=None)
         print("all predictions" , predictions.asciiFile)
 
