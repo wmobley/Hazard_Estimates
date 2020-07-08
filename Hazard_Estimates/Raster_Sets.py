a5daf6a91a03b410d10cdaba3bf00078ee57c83c
wmobley <wmobley@tamu.edu>
2020-07-08 09:45:25 -0500

    Update Raster_Sets.py

--- Hazard_Estimates/Raster_Sets.py
+++ Hazard_Estimates/Raster_Sets.py
@@ -94,7 +94,7 @@ class raster_sets:
                                                      np.where(data[ignore_column] != nodata,
                                                          model_structure.model.predict_proba(data.values)[:, 1], -9999))
                                                     , axis=None)
-            if annualize==True:
+            if annualize:
                 print(annualize)
                 predictions.asciiFile = np.where(predictions.asciiFile != -9999, 1 - (1 - predictions.asciiFile) ** (1 / (model_structure.max - model_structure.min + 1 )), -9999)
             else:
