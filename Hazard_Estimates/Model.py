fb900bfe1ea8943ab791f0f32c27eb7e60b3b3ec
wmobley <wmobley@tamu.edu>
2020-07-18 13:09:23 -0500

    Update Model.py

--- Hazard_Estimates/Model.py
+++ Hazard_Estimates/Model.py
@@ -135,7 +135,7 @@ class model_framework:
         # Y_ = Y_.replace([np.inf, -np.inf, np.nan], 0)
 
         try:
-
+            print("subdivided:",X_.occupancy.unique())
             self.model[key].fit(X_[self.XColumns], X_["y_col"])
         except:
             print(X_["y_col"])
