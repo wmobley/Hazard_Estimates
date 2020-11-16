2710177aea023441874ab1accfb2b21e93d56141
wmobley <wmobley@tamu.edu>
2020-11-16 16:04:24 -0600

    Update XY_Dataset.py

--- Hazard_Estimates/XY_Dataset.py
+++ Hazard_Estimates/XY_Dataset.py
@@ -34,13 +34,13 @@ class X_Y:
 
         claims_df = claims_df.rename(columns={'x': "X", 'y': 'Y', })
 
-        structure_sample = df.loc[df[data_structure.YColumn] == 0]
+
         columns = ['X', 'Y', data_structure.YColumn, 'year_of_loss', 'huc8']
-        structure_sample = structure_sample.sample(n=len(sample), replace=True, random_state=42)
-        structure_sample['year_of_loss'] = structure_sample.apply(
+        df = df.sample(n=len(claims_df), replace=True, random_state=42)
+        df['year_of_loss'] = structure_sample.apply(
             lambda x: rand.randint(claims_df.year_of_loss.min(), claims_df.year_of_loss.max()), axis=1)
 
-        return self.create_categorical_samples(pd.concat([claims_df[columns], structure_sample[columns]]), data_structure.YColumn, True)
+        return self.create_categorical_samples(pd.concat([claims_df[columns], df[columns]]), data_structure.YColumn, True)
 
     def flood_event_dataset_setup(self, data_structure, aggregated, hazardStructure, ):
         '''
