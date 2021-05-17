99d5f447062a709ecd5ee5259517a846a3061e0a
wmobley <wmobley@tamu.edu>
2021-05-17 13:32:38 -0500

    Update XY_Dataset.py

--- Hazard_Estimates/XY_Dataset.py
+++ Hazard_Estimates/XY_Dataset.py
@@ -37,8 +37,9 @@ class X_Y:
 
 
         columns = ['X', 'Y', data_structure.YColumn, 'year_of_loss', 'huc8']
+        print(sample)
         if sample:
-            print(sample)
+
             structure_sample = df.sample(n=len(claims_df), replace=True, random_state=42)
         else:
             structure_sample = df
